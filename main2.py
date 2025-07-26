6fc55603502946bc014b737134a2b1ca9b94861c
Dalton Kato Gomes <dalton.kato.gomes@ericsson.com>
2025-07-26 03:52:49 +0100

    retirado aguarda 1 seg

--- main2.py
+++ main2.py
@@ -27,13 +27,16 @@ edited_df = st.data_editor(
     key="editor"
 )
 
-# Detectar se houve mudança nos campos editáveis
-if not edited_df[edit_columns].equals(st.session_state.previous_values):
-    #time.sleep(1)  # Esperar 1 segundo antes de salvar
-    with pd.ExcelWriter(a_uploaded_file, engine="openpyxl", mode="a", if_sheet_exists="replace") as writer:
-        edited_df.to_excel(writer, index=False, sheet_name="Sheet1")
-    st.session_state.previous_values = edited_df[edit_columns].copy()
-    st.success("✅ Alterações salvas automaticamente.")
+# Botão para salvar alterações
+if st.button("Salvar Alterações"):
+    if not edited_df[edit_columns].equals(st.session_state.previous_values):
+        time.sleep(1)
+        with pd.ExcelWriter(a_uploaded_file, engine="openpyxl", mode="a", if_sheet_exists="replace") as writer:
+            edited_df.to_excel(writer, index=False, sheet_name="Sheet1")
+        st.session_state.previous_values = edited_df[edit_columns].copy()
+        st.success("✅ Alterações salvas com sucesso.")
+    else:
+        st.info("Nenhuma alteração detectada.")
 
 # Botão para gerar classificação
 if st.button("Atualizar Classificação e Ranking"):
